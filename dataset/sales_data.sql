b00a7f6deca5f17bbc564e3f474ecf256639fa68
radhaajaymishra-sudo <radhaajaymishra@gmail.com>
2026-04-08 10:33:48 +0530

    Update sales_data.sql

--- dataset/sales_data.sql
+++ dataset/sales_data.sql
@@ -7,7 +7,10 @@
 INSERT INTO Customers VALUES
 (1, 'Rahul Sharma', 'Delhi'),
 (2, 'Anita Verma', 'Mumbai'),
-(3, 'Amit Singh', 'Bangalore');
+(3, 'Amit Singh', 'Bangalore'),
+(4, 'Neha Gupta', 'Delhi'),
+(5, 'Rohit Jain', 'Pune');
+  
 
 INSERT INTO Products VALUES
 (1, 'Laptop', 'Electronics', 60000),
@@ -17,10 +20,13 @@ INSERT INTO Products VALUES
 INSERT INTO Orders VALUES
 (101, 1, '2024-01-10', 80000),
 (102, 2, '2024-02-15', 20000),
-(103, 3, '2024-03-20', 22000);
+(103, 3, '2024-03-20', 22000),
+(104, 4, '2024-04-10', 15000),
+(105, 5, '2024-05-12', 30000) ;
 
 INSERT INTO Order_Details VALUES
 (101, 1, 1),
 (101, 3, 10),
 (102, 2, 1),
 (103, 3, 11);
+
